13fd3f53411a6b1777dbe5cb6c2b983e1796cee9
Gagandeep Kaur <71839854+a00224734@users.noreply.github.com>
2020-09-26 01:22:27 +0530

    Add files via upload

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        public static double DoOperation(double n1, double n2, string opr)
+        {
+            double result = double.NaN;
+
+            switch(opr)
+            {
+                case "add":
+                    result = n1 + n2;
+                    break;
+                case "sub":
+                    result = n1 - n2;
+                    break;
+                case "mul":
+                    result = n1 * n2;
+                    break;
+                case "div":
+                    if (n2 != 0)
+                    {
+                        result = n1 / n2;
+                    }
+                    else Console.WriteLine("Enter a non-zero value.");
+                    break;
+                default:
+                    Console.WriteLine("You have entered an incorrect option.");
+                    break;
+            }
+            return result;
+        }
+    }
+}
